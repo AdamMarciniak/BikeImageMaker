d0ff27fe32ef51b133f2e0cc5d023af4f3fc68bb
Adam Marciniak <adam668@gmail.com>
2024-02-01 13:32:28 -0800

    Add gitignore

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const img = new Image();
-img.src = `colortest.png`;
+img.src = `circle.png`;
 const width = 800;
 const height = 500;
 
